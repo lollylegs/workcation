5ab1cfb59e0469a88069b312c33906500b720d42
Marea Norton <nortonmarea@gmail.com>
2021-01-13 02:53:42 +1030

    Finish workcation chapter 4

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,171 @@
 <template>
-	<div id="app" class="min-h-scree bg-gray-200 antialias">
-		<SiteHeader/>
-		<SearchFilters/>
-		<main class="py-6">
-			<div class="px-4">
-				<h3 class="text-gray-900 text-xl">Los Angeles</h3>
-				<p class="text-gray-600">Live like the stars in these luxurious Southern Californian Estates.</p>
-			</div>
-			<div class="mt-6 px-2">
-				<div class="sm:flex sm:-ml-2 sm:overflow-x-auto sm:pb-8 sm:pr-2">
-					<div v-for="i in 4" :key="i" :class="{'mt-10': i > 1}" class="sm:mt-0 sm:max-w-xs sm:w-full sm:flex-shrink-0 sm:px-2">
-					<div class="relative pb-5/6">
-						<img class="absolute inset-0 h-full rounded-lg shadow-md object-cover" src="/img/losAngeles.jpeg" alt="Los Angeles Modern House">
-					</div>
-					<div class="relative px-4 -mt-16">
-						<div class="bg-white rounded-lg px-4 py-4 shadow-lg">
-							<div class="flex">
-								<span class="inline-block px-2 py-1 leading-none bg-teal-200 text-teal-800 rounded-full font-semibold uppercase tracking-wide text-xs">Plus</span>
-								<div class="ml-2 text-xs text-gray-600 font-semibold uppercase tracking-wide">
-									3beds &bull; 2baths
-								</div>
-							</div>
-							<h4 class="mt-1 text-gray-900 font-semibold text-lg">Modern home in city center</h4>
-							<div class="mt-1">
-								<span class="text-gray-900">$1,400</span>
-								<span class="ml-1 text-sm text-gray-500">/wk</span>
-							</div>
-							<div class="mt-2 flex items-center text-sm text-gray-600">
-							<svg class="h-4 w-4 fill-current text-teal-500" viewBox="0 0 24 24" ><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z" />
-							</svg>
-							<svg class="h-4 w-4 fill-current text-teal-500" viewBox="0 0 24 24" ><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z" />
-							</svg>
-							<svg class="h-4 w-4 fill-current text-teal-500" viewBox="0 0 24 24" ><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z" />
-							</svg>
-							<svg class="h-4 w-4 fill-current text-teal-500" viewBox="0 0 24 24" ><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z" />
-							</svg>
-							<svg class="h-4 w-4 fill-current text-teal-500" viewBox="0 0 24 24" ><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z" />
-							</svg>
-							<span class="ml-2">34 Reviews</span>
-							</div>
-						</div>
-					</div>
-				</div>
-				</div>
-				
-			</div>
-		</main>
-	</div>
+  <div id="app" class="min-h-screen bg-gray-200 antialiased xl:flex xl:flex-col xl:h-screen">
+    <SiteHeader class="xl:flex-shrink-0"/>
+    <div class="xl:flex-1 xl:flex xl:overflow-y-hidden">
+      <SearchFilters/>
+      <main class="py-6 xl:flex-1 xl:overflow-x-hidden">
+        <div v-for="(location, i) in locations" :key="i" :class="{'mt-6': i > 0}">
+          <div class="px-4 xl:px-8">
+            <h3 class="text-gray-900 text-xl">{{ location.title }}</h3>
+            <p class="text-gray-600">{{ location.description }}</p>
+          </div>
+          <div class="mt-6 sm:overflow-x-auto sm:overflow-y-hidden">
+            <div class="px-4 sm:inline-flex sm:pt-2 sm:pb-8 xl:px-8">
+              <div v-for="(property, i) in location.properties" :key="i" :class="{'mt-10 sm:ml-4': i > 0 }" class="sm:mt-0 sm:w-80 sm:flex-shrink-0">
+                <PropertyCard :property="property"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
 </template>
 
 <script>
 import SiteHeader from '@/components/SiteHeader'
 import SearchFilters from '@/components/SearchFilters'
+import PropertyCard from '@/components/PropertyCard'
 
 export default {
-	name: 'app',
-	components: {
-		SiteHeader,
-		SearchFilters,
-	}
+  name: 'app',
+  components: {
+    SiteHeader,
+    SearchFilters,
+    PropertyCard,
+  },
+  data() {
+    return {
+      locations: [
+        {
+          title: 'Los Angeles',
+          description: "Live like the stars in these luxurious Southern California estates.",
+          properties: [
+            {
+              imageUrl: 'https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+              beds: 3,
+              baths: 2,
+              title: "Modern home in city center",
+              price: 190000,
+              reviewCount: 34,
+              rating: 4,
+            },
+            {
+              imageUrl: 'https://images.unsplash.com/photo-1513584684374-8bab748fbf90?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+              beds: 4,
+              baths: 1,
+              title: "Quiet living outside the city",
+              price: 175000,
+              reviewCount: 12,
+              rating: 3,
+            },
+            {
+              imageUrl: 'https://images.unsplash.com/photo-1502005229762-cf1b2da7c5d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80',
+              beds: 5,
+              baths: 4,
+              title: "Renovated apartment uptown",
+              price: 200000,
+              reviewCount: 54,
+              rating: 5,
+            },
+            {
+              imageUrl: 'https://images.unsplash.com/photo-1505691938895-1758d7feb511?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+              beds: 3,
+              baths: 2,
+              title: "Family home in the suburbs",
+              price: 115000,
+              reviewCount: 34,
+              rating: 4,
+            },
+          ]
+        },
+        {
+          title: 'Phoenix',
+          description: "Escape the cold and enjoy great weather without breaking the bank.",
+          properties: [
+            {
+              imageUrl: 'https://images.unsplash.com/photo-1447958272669-9c562446304f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80',
+              beds: 3,
+              baths: 2,
+              title: "Modern home in city center",
+              price: 190000,
+              reviewCount: 34,
+              rating: 4,
+            },
+            {
+              imageUrl: 'https://images.unsplash.com/photo-1475855581690-80accde3ae2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80',
+              beds: 4,
+              baths: 1,
+              title: "Quiet living outside the city",
+              price: 175000,
+              reviewCount: 12,
+              rating: 3,
+            },
+            {
+              imageUrl: 'https://images.unsplash.com/photo-1472224371017-08207f84aaae?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+              beds: 5,
+              baths: 4,
+              title: "Renovated apartment uptown",
+              price: 200000,
+              reviewCount: 54,
+              rating: 5,
+            },
+            {
+              imageUrl: 'https://images.unsplash.com/photo-1484154218962-a197022b5858?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+              beds: 3,
+              baths: 2,
+              title: "Family home in the suburbs",
+              price: 115000,
+              reviewCount: 34,
+              rating: 4,
+            },
+          ]
+        },
+        {
+          title: 'Dallas',
+          description: "Experience Texas living in these awesome ranch-style homes.",
+          properties: [
+            {
+              imageUrl: 'https://images.unsplash.com/photo-1471231681582-352356ab45a0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80',
+              beds: 3,
+              baths: 2,
+              title: "Modern home in city center",
+              price: 190000,
+              reviewCount: 34,
+              rating: 4,
+            },
+            {
+              imageUrl: 'https://images.unsplash.com/photo-1533779283484-8ad4940aa3a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80',
+              beds: 4,
+              baths: 1,
+              title: "Quiet living outside the city",
+              price: 175000,
+              reviewCount: 12,
+              rating: 3,
+            },
+            {
+              imageUrl: 'https://images.unsplash.com/photo-1432303492674-642e9d0944b2?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+              beds: 5,
+              baths: 4,
+              title: "Renovated apartment uptown",
+              price: 200000,
+              reviewCount: 54,
+              rating: 5,
+            },
+            {
+              imageUrl: 'https://images.unsplash.com/photo-1560185007-c5ca9d2c014d?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+              beds: 3,
+              baths: 2,
+              title: "Family home in the suburbs",
+              price: 115000,
+              reviewCount: 34,
+              rating: 4,
+            },
+          ]
+        }
+      ],
+    }
+  }
 }
 </script>
 
--- src/components/SearchFilters.vue
+++ src/components/SearchFilters.vue
@@ -1,117 +1,118 @@
 <template>
-	<section class="bg-gray-600">
-		<div class="flex justify-between px-4 py-3">
-			<div class="relative max-w-xs w-full">
-				<div class="absolute inset-y-0 left-0 flex items-center pl-3">
-					<img class="h-6 w-6" src="/img/search-line.svg">
-				</div>
-				<input class="block w-full bg-gray-800 focus:outline:none focus:bg-white focus:text-gray-900 rounded-lg pl-10 pr-4 py-2" placeholder="Search by keywords">
-			</div>
-			<button @click="toggle" type="button" class="ml-4 inline-flex bg-gray-700 hover:text-white hover:bg-gray-500 focus:outline-none focus:shadow-outline rounded-lg shadow pl-3 pr-4 pt-1">
-				<img class="h-8 w-8" src="/img/filter-3-fill.svg">
-				<span class="pt-1 pr-4 text-gray-200 font-medium">Filters</span>
-			</button>
-		</div>
-		<form v-show=isOpen>
-			<div class="px-4 py-4 border-t border-gray-900 sm:flex sm:-mx-2">
-				<div class="flex sm:w-1/2 sm:mx-0">
-					<label class="block w-1/2 px-2">
-						<span class="text-sm font-semibold text-gray-400">Bedrooms</span>
-						<select class="mt-1 form-select block w-full py-2 bg-gray-700 rounded-lg text-white shadow focus:bg-gray-500">
-							<option>4</option>
-						</select>
-					</label>
-					<label class="block w-1/2 px-2">
-						<span class="text-sm font-semibold text-gray-400">Bathrooms</span>
-						<select class="mt-1 form-select block w-full py-2 bg-gray-700 text-white rounded-lg shadow focus:bg-gray-500">
-							<option>2</option>
-						</select>
-					</label>
-				</div>
-				<div class="mt-4 sm:w-1/2 sm:mt-0 sm:px-2">
-					<label>
-						<span class="text-sm font-semibold text-gray-400">Price Range</span>
-						<select class="mt-1 form-select block w-full py-2 bg-gray-700 text-sm text-white rounded-lg shadow focus:bg-gray-500">
-							<option>Up to $2,000/wk</option>
-						</select>
-					</label>
-				</div>
-			</div>
-				<div class="px-4 py-4 border-t border-gray-900">
-					<span class="block text-sm font-semibold text-gray-400">Property Type</span>
-					<div class="sm:flex sm:-mx-2">
-						<label class="mt-2 sm:w-1/4 sm:px-2 flex items-center">
-							<input  class="form-radio bg-gray-800 focus:bg-gray-700" type="radio" name="propertyType" value="house">
-							<span class="ml-2 text-white text-sm">House</span>
-						</label>
-						<label class="mt-2 sm:w-1/4 sm:px-2 flex items-center">
-							<input class="form-radio bg-gray-800 focus:bg-gray-700" type="radio" name="propertyType" value="apartment">
-							<span class="ml-2 text-white text-sm">Apartment</span>
-						</label>
-						<label class="mt-2 sm:w-1/4 sm:px-2 flex items-center">
-							<input class="form-radio bg-gray-800 focus:bg-gray-700" type="radio" name="propertyType" value="loft">
-							<span class="ml-2 text-white text-sm">Loft</span>
-						</label>
-						<label class="mt-2 sm:w-1/4 sm:px-2 flex items-center">
-							<input class="form-radio bg-gray-800 focus:bg-gray-700" type="radio" name="propertyType" value="townhouse">
-							<span class="ml-2 text-white text-sm">Townhouse</span>
-						</label>
-					</div>
-				</div>
-				<div class="px-4 py-4 border-t border-gray-900">
-					<span class="block font-semibold text-gray-400">Amenities</span>
-					<div class="sm:flex sm:-mx-2 sm:flex-wrap">
-						<label class="mt-2 flex items-center sm:w-1/4 sm:px-2">
-							<input class="bg-gray-800 focus:bg-gray-700 form-checkbox" type="checkbox" name="balcony">
-							<span class="ml-2 text-white text-sm">Balcony</span>
-						</label>
-						<label class="mt-2 flex items-center sm:w-1/4 sm:px-2">
-							<input class="bg-gray-800 focus:bg-gray-700 form-checkbox" type="checkbox" name="pool">
-							<span class="ml-2 text-white text-sm">Pool</span>
-						</label>
-						<label class="mt-2 flex items-center sm:w-1/4 sm:px-2">
-							<input class="bg-gray-800 focus:bg-gray-700 form-checkbox" type="checkbox" name="beach">
-							<span class="ml-2 text-white text-sm">Beach</span>
-						</label>
-						<label class="mt-2 flex items-center sm:w-1/4 sm:px-2">
-							<input class="bg-gray-800 focus:bg-gray-700 form-checkbox" type="checkbox" name="petFriendly">
-							<span class="ml-2 text-white text-sm">Pet Friendly</span>
-						</label>
-						<label class="mt-2 flex items-center sm:w-1/4 sm:px-2">
-							<input class="bg-gray-800 focus:bg-gray-700 form-checkbox" type="checkbox" name="kidFriendly">
-							<span class="ml-2 text-white text-sm">Kid friendly</span>
-						</label>
-						<label class="mt-2 flex items-center sm:w-1/4 sm:px-2">
-							<input class="bg-gray-800 focus:bg-gray-700 form-checkbox" type="checkbox" name="parking">
-							<span class="ml-2 text-white text-sm">Parking</span>
-						</label>
-						<label class="mt-2 flex items-center sm:w-1/2 sm:px-2">
-							<input class="bg-gray-800 focus:bg-gray-700 form-checkbox" type="checkbox" name="airConditioning">
-							<span class="ml-2 text-white text-sm">Air Conditioning</span>
-						</label>
-					</div>
-				</div>
-				<div class="bg-gray-800 ppx-4 py-4 sm:text-right">
-					<button class="block w-full sm:w-auto sm:inline-block mr-4 bg-teal-600 hover:bg-teal-500 font-semibold text-white px-4 py-2 rounded-lg">Update results</button>
-				</div>
-		</form>
-	</section>	
+  <section class="bg-gray-800 xl:w-72">
+    <div class="flex justify-between px-4 py-3 xl:hidden">
+      <div class="relative max-w-xs w-full">
+        <div class="absolute inset-y-0 left-0 flex items-center pl-3">
+          <svg class="h-6 w-6 fill-current text-gray-600" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.32 14.9l1.1 1.1c.4-.02.83.13 1.14.44l3 3a1.5 1.5 0 0 1-2.12 2.12l-3-3a1.5 1.5 0 0 1-.44-1.14l-1.1-1.1a8 8 0 1 1 1.41-1.41l.01-.01zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/></svg>
+        </div>
+        <input class="block w-full bg-gray-900 focus:outline-none focus:bg-white focus:text-gray-900 text-white rounded-lg pl-10 pr-4 py-2" placeholder="Search by keywords">
+      </div>
+      <button type="button" class="ml-4 inline-flex items-center hover:bg-gray-600 focus:outline-none focus:shadow-outline rounded-lg shadow pl-3 pr-4"
+        @click="toggle"
+        :class="{ 'bg-gray-600': isOpen, 'bg-gray-700': !isOpen }"
+      >
+        <svg class="h-6 w-6 fill-current text-gray-500" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M3 6a1 1 0 0 1 1-1h16a1 1 0 1 1 0 2H4a1 1 0 0 1-1-1zm3 6a1 1 0 0 1 1-1h10a1 1 0 1 1 0 2H7a1 1 0 0 1-1-1zm4 5a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-4z"/></svg>
+        <span class="ml-1 text-white font-medium">Filters</span>
+      </button>
+    </div>
+    <form :class="{ 'hidden': !isOpen, 'block': isOpen }" class="xl:block xl:h-full xl:flex xl:flex-col xl:justify-between">
+      <div class="lg:flex xl:block xl:overflow-y-auto">
+        <div class="px-4 py-4 border-t border-gray-900 lg:w-1/3 xl:border-t-0 xl:w-full">
+          <div class="flex flex-wrap -mx-2">
+            <label class="block w-1/2 px-2 sm:w-1/4 lg:w-1/2">
+              <span class="text-sm font-semibold text-gray-500">Bedrooms</span>
+              <select class="mt-1 form-select block w-full rounded-lg text-white shadow bg-gray-500 focus:bg-gray-600">
+                <option>4</option>
+              </select>
+            </label>
+            <label class="block w-1/2 px-2 sm:w-1/4 lg:w-1/2">
+              <span class="text-sm font-semibold text-gray-500">Bathrooms</span>
+              <select class="mt-1 form-select block w-full rounded-lg text-white shadow bg-gray-500 focus:bg-gray-600">
+                <option>2</option>
+              </select>
+            </label>
+            <label class="mt-4 block w-full px-2 sm:mt-0 sm:w-1/2 lg:mt-4 lg:w-full">
+              <span class="text-sm font-semibold text-gray-500">Price Range</span>
+              <select class="mt-1 form-select block w-full rounded-lg text-white shadow bg-gray-500 focus:bg-gray-600">
+                <option>Up to $2,000/wk</option>
+              </select>
+            </label>
+          </div>
+        </div>
+        <div class="px-4 py-4 border-t border-gray-900 lg:w-1/3 lg:border-l xl:w-full">
+          <span class="block text-sm font-semibold text-gray-500">Property Type</span>
+          <div class="sm:flex sm:-mx-2 lg:block lg:mx-0">
+            <label class="mt-3 sm:w-1/4 sm:px-2 flex items-center lg:w-full lg:px-0">
+              <input class="form-radio bg-gray-900 focus:bg-gray-700" type="radio" name="propertyType" value="house">
+              <span class="ml-2 text-white">House</span>
+            </label>
+            <label class="mt-3 sm:w-1/4 sm:px-2 flex items-center lg:w-full lg:px-0">
+              <input class="form-radio bg-gray-900 focus:bg-gray-700" type="radio" name="propertyType" value="apartment">
+              <span class="ml-2 text-white">Apartment</span>
+            </label>
+            <label class="mt-3 sm:w-1/4 sm:px-2 flex items-center lg:w-full lg:px-0">
+              <input class="form-radio bg-gray-900 focus:bg-gray-700" type="radio" name="propertyType" value="loft">
+              <span class="ml-2 text-white">Loft</span>
+            </label>
+            <label class="mt-3 sm:w-1/4 sm:px-2 flex items-center lg:w-full lg:px-0">
+              <input class="form-radio bg-gray-900 focus:bg-gray-700" type="radio" name="propertyType" value="townhouse">
+              <span class="ml-2 text-white">Townhouse</span>
+            </label>
+          </div>
+        </div>
+        <div class="px-4 py-4 border-t border-gray-900 lg:w-1/3 lg:border-l xl:w-full">
+          <span class="block text-sm font-semibold text-gray-500">Amenities</span>
+          <div class="sm:flex sm:-mx-2 sm:flex-wrap">
+            <label class="mt-3 flex items-center sm:w-1/4 sm:px-2 lg:w-1/2 xl:w-full">
+              <input class="form-checkbox bg-gray-900 focus:bg-gray-700" type="checkbox" name="balcony">
+              <span class="ml-2 text-white">Balcony</span>
+            </label>
+            <label class="mt-3 flex items-center sm:w-1/4 sm:px-2 lg:w-1/2 xl:w-full">
+              <input class="form-checkbox bg-gray-900 focus:bg-gray-700" type="checkbox" name="pool">
+              <span class="ml-2 text-white">Pool</span>
+            </label>
+            <label class="mt-3 flex items-center sm:w-1/4 sm:px-2 lg:w-1/2 xl:w-full">
+              <input class="form-checkbox bg-gray-900 focus:bg-gray-700" type="checkbox" name="beach">
+              <span class="ml-2 text-white">Beach</span>
+            </label>
+            <label class="mt-3 flex items-center sm:w-1/4 sm:px-2 lg:w-1/2 xl:w-full">
+              <input class="form-checkbox bg-gray-900 focus:bg-gray-700" type="checkbox" name="petFriendly">
+              <span class="ml-2 text-white">Pet friendly</span>
+            </label>
+            <label class="mt-3 flex items-center sm:w-1/4 sm:px-2 lg:w-1/2 xl:w-full">
+              <input class="form-checkbox bg-gray-900 focus:bg-gray-700" type="checkbox" name="kidFriendly">
+              <span class="ml-2 text-white">Kid friendly</span>
+            </label>
+            <label class="mt-3 flex items-center sm:w-1/4 sm:px-2 lg:w-1/2 xl:w-full">
+              <input class="form-checkbox bg-gray-900 focus:bg-gray-700" type="checkbox" name="parking">
+              <span class="ml-2 text-white">Parking</span>
+            </label>
+            <label class="mt-3 flex items-center sm:w-1/2 sm:px-2 lg:w-full">
+              <input class="form-checkbox bg-gray-900 focus:bg-gray-700" type="checkbox" name="airConditioning">
+              <span class="ml-2 text-white">Air conditioning</span>
+            </label>
+          </div>
+        </div>
+      </div>
+      <div class="bg-gray-900 px-4 py-4 sm:text-right">
+        <button class="block w-full sm:w-auto sm:inline-block bg-teal-700 hover:bg-teal-500 font-semibold text-white px-4 py-2 rounded-lg xl:block xl:w-full">Update results</button>
+      </div>
+    </form>
+  </section>
 </template>
 
 <script>
 export default {
-	props: [],
-	data() {
-		return {
-          isOpen: false,
-		}
-	},
-	methods: {
-        toggle() {
-           this.isOpen = !this.isOpen        	
-        }
-	},
+  props: [],
+  data() {
+    return {
+      isOpen: true,
+    }
+  },
+  methods: {
+    toggle() {
+      this.isOpen = !this.isOpen
+    }
+  },
 }
-  
 </script>
-
