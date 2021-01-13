6b4c2162856716690b1bd35f374b5a919acc073e
Marea Norton <nortonmarea@gmail.com>
2021-01-13 13:47:17 +1030

    Finish final workcation project

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -27,15 +27,22 @@
           <a href="#" class="mt-1 block px-3 py-1 rounded font-semibold text-white hover:bg-gray-800 sm:mt-0 sm:text-sm sm:px-2 sm:ml-2 xl:text-gray-900 xl:hover:bg-gray-200">Messages</a>
         </div>
         <div class="relative px-5 py-5 sm:py-0 sm:ml-4 sm:px-0">
-          <div class="flex">
-            <img class="xl:h-12 xl:w-12 object-cover rounded-full border-2 border-gray-600 sm:h-8 sm:w-8 xl:border-gray-300" src="/img/girl_2.jpg" alt="">
-            <span class="ml-4 font-semibold text-gray-200 sm:hidden">Marea Norton</span>
-          </div>
-          <div class="mt-5 sm:hidden">
-            <a href="#account" class="block text-gray-400 hover:text-white">Account settings</a>
-            <a href="#support" class="mt-3 block text-gray-400 hover:text-white">Support</a>
-            <a href="#sign-out" class="mt-3 block text-gray-400 hover:text-white">Sign out</a>
-          </div>
+          <Dropdown class="hidden sm:block">
+            <template #trigger="{ hasFocus, isOpen }">
+              <span class="block h-8 w-8 overflow-hidden rounded-full border-2 "
+                :class="[(hasFocus || isOpen) ? 'border-white xl:border-indigo-500' : 'border-gray-600 xl:border-gray-300']"
+              >
+                <img class="h-full w-full object-cover" src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&q=80" alt="">
+              </span>
+            </template>
+            <template #dropdown>
+              <div class="mt-3 bg-white xl:border rounded-lg w-48 py-2 shadow-xl">
+                <a href="#account" class="block hover:text-white text-gray-800 px-4 py-2 hover:bg-indigo-500">Account settings</a>
+                <a href="#support" class="block hover:text-white text-gray-800 mt-0 px-4 py-2 hover:bg-indigo-500">Support</a>
+                <a href="#sign-out" class="block hover:text-white text-gray-800 mt-0 px-4 py-2 hover:bg-indigo-500">Sign out</a>
+              </div>
+            </template>
+          </Dropdown>
          </div>
       </div>
     </nav>
@@ -43,8 +50,9 @@
 </template>
 
 <script>
+import Dropdown from '@/components/Dropdown'  
 export default {
-  components: {},
+  components: { Dropdown },
   props: [],
   data() {
     return {
